5d974341bb53509e7ec750a14f58d72b318438d5
Jerry Kuo <j.kuo1995@gmail.com>
2023-06-05 15:33:03 -0700

    Feature 5 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_home(request):
+    return redirect("home")
+
 
 urlpatterns = [
+    path("", redirect_home),
+    path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
 ]
