5e52c1f162f0bcdcd7e0c595b4a6bea8271d75fc
Jerry Kuo <j.kuo1995@gmail.com>
2023-06-06 12:59:51 -0700

    Feature 15 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -24,6 +24,7 @@ def redirect_home(request):
 
 
 urlpatterns = [
+    path("tasks/", include("tasks.urls")),
     path("accounts/", include("accounts.urls")),
     path("", redirect_home, name="home"),
     path("projects/", include("projects.urls")),
